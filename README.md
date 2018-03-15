aade638d2dbb265f44ab52b672b777cd40ad3858
WISEPLAT <stroy-detal.com@yandex.ru>
2018-03-15 13:41:15 +0300

    1.0.2

--- README.md
+++ README.md
@@ -1,1 +1,51 @@
 # npm1-wiseplat-keyfile-recognizer
+
+[![Build Status](https://travis-ci.org/wiseplat/npm1-wiseplat-keyfile-recognizer.svg?branch=master)](https://travis-ci.org/wiseplat/npm1-wiseplat-keyfile-recognizer)
+
+Checks for structural sanity (key-names and value-types) of `json`-keyfiles.
+
+Currently recognized keyfiles:
+ - Wisesale
+ - [web3-secret-storage](https://github.com/wiseplat/wiki/wiki/Web3-Secret-Storage-Definition) (v3)
+  - scrypt
+  - pbkdf2
+
+## Installation
+
+```shell
+npm install --save npm1-wiseplat-keyfile-recognizer
+```
+
+## Usage
+
+```javascript
+var fs = require('fs');
+var recognizer = require('npm1-wiseplat-keyfile-recognizer');
+
+fs.readFile('keyfile.json', (err, data) => {
+    var json = JSON.parse(data);
+    var result = recognizer(json);
+    /** result
+     *               [ 'web3', 3 ]   web3 (v3) keyfile
+     *  [ 'wisesale', undefined ]   Wisesale keyfile
+     *                        null     invalid keyfile
+     */
+}));
+```
+
+## Development
+
+To build and run the tests:
+
+```shell
+$ npm install
+$ npm test
+```
+
+## Contributions
+
+Contributions welcome - see [CONTRIBUTING.md](CONTRIBUTING.md)
+
+## License
+
+MIT - see [LICENSE.md](LICENSE.md)
---
+++ src/index.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const _ = require('underscore');
+const compare = require('json-structure-diff').compareJSONObjects;
+
+const structureJsonRaw = require('./keyfiles-structure.json');
+const structureJson = toLower(structureJsonRaw);
+
+/**
+ * Deep copies JSON object with keys.toLowerCase()
+ * @param  {Object} obj input
+ * @return {Object}     output
+ */
+function toLower(obj) {
+    var newObj = {};
+    _.keys(obj).forEach((key) => {
+        if (typeof(newObj[key.toLowerCase()] = obj[key]) === 'object')
+            newObj[key] = toLower(newObj[key])
+    });
+    return newObj
+}
+
+/**
+ * Matches the input object's structure against valid keyfile structures
+ * (respects key and value-type)
+ * @param  {Object} obj input keyfile object
+ * @return {[String, Number]} array of type (web3|wisesale) and version
+ */
+module.exports = function(obj) {
+    var result = null;
+    try {
+        _.keys(structureJson).forEach((structureType) => {
+            var structure = {
+                parent: structureType,
+                content: structureJson[structureType]['json']
+            }
+            var instance = {
+                parent: 'obj',
+                content: toLower(obj)
+            }
+            var error = compare([structure, instance]);
+            if (!error) {
+                var match = structureJson[structure.parent];
+                result = [ match.type, match.json.version ];
+                throw BreakException;
+            }
+        });
+    } catch (e) {}
+    return (result) ? result : null
+}
